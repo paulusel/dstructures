6fa7b4b9b8ea155d5c3587cf0f646e99df27ef5a
paulusel <paulusel@proton.me>
2025-05-04 21:54:43 +0300

    feat: support removing in unordered for unordered vectors

--- include/dstruct/vector.h
+++ include/dstruct/vector.h
@@ -13,13 +13,14 @@ struct vector {
 typedef struct vector vector;
 
 vector* vector_create(size_t elem_size, size_t count);
-vector* vector_duplicate(vector *v);
+vector* vector_clone(vector *v);
 
 void vector_push_back(vector *v, void *val);
 void vector_pop_back(vector *v);
-void vector_append(vector *v, void *val, size_t count);
 
 void* vector_at(vector *v, size_t pos);
 void* vector_back(vector *v);
+
+void vector_remove_unordered(vector *v, size_t pos);
 
 void vector_destroy(vector *v);
