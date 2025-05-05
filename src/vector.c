77ff61a4b23fd2387bfe69168bc4cb0473557efc
paulusel <paulusel@proton.me>
2025-05-05 17:09:19 +0300

    refactor: remove unclear procedure

--- src/vector.c
+++ src/vector.c
@@ -52,48 +52,34 @@ vector* vector_clone(vector *v) {
 }
 
 void vector_push_back(vector *v, void *val) {
-    if(!v->data) return;
     consider_expanding(v);
-
-    void* target = (char*)v->data + v->elem_size*v->size;
+    void* target = pointer_to(v, v->size);
     memcpy(target, val, v->elem_size);
     ++v->size;
 }
 
-void vector_append(vector *v, void *val, size_t count){
-    int new_size = v->size + count;
-    if(v->capacity < new_size) {
-        v->capacity = new_size * 2;
-        v->data = realloc(v->data, v->capacity*v->elem_size);
-    }
-
-    void* target = (char*)v->data + v->size*v->elem_size;
-    memcpy(target, val, v->elem_size*count);
-    v->size += count;
-}
-
 void vector_pop_back(vector *v) {
-    if(!v->data) return;
+    if(v->size == 0) return;
     --v->size;
-    if(v->size * 2 < v->capacity) consider_shrinking(v);
+    consider_shrinking(v);
 }
 
 void* vector_at(vector *v, size_t pos) {
-    if(!v->data || pos < 0 || pos >= v->size) return NULL;
+    if(pos < 0 || pos >= v->size) return NULL;
     return pointer_to(v, pos);
 }
 
 void* vector_back(vector* v) {
-    return vector_at(v, v->size - 1);
+    return pointer_to(v, v->size-1);
 }
 
 void vector_remove_unordered(vector *v, size_t pos) {
-    if(!v->data || pos < 0 || pos >= v->size) return;
+    if(pos < 0 || pos >= v->size) return;
 
     void *target = pointer_to(v, pos);
     void *end = pointer_to(v, v->size-1);
 
-    if(target != end) memcpy(target, end, v->size);
+    if(target != end) memcpy(target, end, v->elem_size);
     --v->size;
     consider_shrinking(v);
 }
